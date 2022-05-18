ac5e9aa9b6b2ad1c277779a97a78b61a4f4d2908
Jantu Deb <jantud54@gmail.com>
2022-05-18 16:34:47 +0530

    cors

--- config/cors-config.js
+++ config/cors-config.js
@@ -1,6 +1,6 @@
 const corsConFig = {
   origin: [
-    "http://localhost:3000","https://ionvu-connect.netlify.app"
+    "http://localhost:3000","https://ionvuconnect.netlify.app/"
   ],
   credentials: true,
 };
