5511ab7d1835e1893989ae6532a0bc22043c8d1d
Jantu Deb <jantud54@gmail.com>
2022-05-24 23:45:50 +0530

    added preview url in cors origin

--- config/cors-config.js
+++ config/cors-config.js
@@ -1,6 +1,6 @@
 const corsConFig = {
   origin: [
-    "http://localhost:3000","https://ionvuconnect.netlify.app"
+    "http://localhost:3000","https://ionvuconnect.netlify.app", "https://thriving-basbousa-5fef94.netlify.app"
   ],
   credentials: true,
 };
